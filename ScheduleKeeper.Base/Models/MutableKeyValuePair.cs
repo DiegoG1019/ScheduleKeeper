14bbc01adb1e0911a0a3a5ef1458fe49b1a8a0d2
Diego García <dagarciam1014@gmail.com>
2021-09-15 00:41:06 -0400

    Create MutableKeyValuePair.cs

---
+++ ScheduleKeeper.Base/Models/MutableKeyValuePair.cs
@@ -0,0 +1,27 @@
+﻿namespace ScheduleKeeper.Base.Models;
+
+public class MutableKeyValuePair<T1, T2> : Contextual
+{
+    private T1? _t1;
+    private T2? _t2;
+
+    public T1? Key
+    {
+        get => _t1;
+        set
+        {
+            _t1 = value;
+            Notify();
+        }
+    }
+
+    public T2? Value
+    {
+        get => _t2;
+        set
+        {
+            _t2 = value;
+            Notify();
+        }
+    }
+}
