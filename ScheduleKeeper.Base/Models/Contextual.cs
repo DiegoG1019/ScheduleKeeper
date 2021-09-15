b2ca986bba93ab45f29693f540d75af59de006ed
Diego García <dagarciam1014@gmail.com>
2021-09-15 00:41:03 -0400

    Create Contextual.cs

---
+++ ScheduleKeeper.Base/Models/Contextual.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScheduleKeeper.Base.Models
+{
+    public abstract class Contextual : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void Notify([CallerMemberName] string? caller = null) => PropertyChanged?.Invoke(this, new(caller));
+    }
+}
