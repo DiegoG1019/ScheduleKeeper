f4840114da9dc9b408e1256a9ba18ee5c5f1ad51
Diego García <dagarciam1014@gmail.com>
2021-09-15 00:41:16 -0400

    Create TimeFramePlan.cs

---
+++ ScheduleKeeper.Base/Models/TimeFramePlan.cs
@@ -0,0 +1,44 @@
+﻿namespace ScheduleKeeper.Base.Models;
+
+public class TimeFramePlan : Contextual
+{
+    private TimeOnly _startTime;
+    private TimeOnly _endTime;
+    private DayOfWeek _dayOfWeek;
+
+    public virtual TimeOnly StartTime
+    {
+        get => _startTime;
+        set
+        {
+            if (_startTime == value)
+                return;
+            _startTime = value; 
+            Notify();
+        }
+    }
+
+    public virtual TimeOnly EndTime 
+    { 
+        get => _endTime; 
+        set
+        {
+            if (_endTime == value)
+                return;
+            _endTime = value; 
+            Notify(); 
+        } 
+    }
+
+    public virtual DayOfWeek OnDay
+    {
+        get => _dayOfWeek; 
+        set 
+        {
+            if (_dayOfWeek == value)
+                return;
+            _dayOfWeek = value;
+            Notify(); 
+        } 
+    }
+}
