4372e3eb8545e1868c8b667bfb3db3756ee0212d
Diego García <dagarciam1014@gmail.com>
2021-09-15 01:53:11 -0400

    Create RangeEnabledObservableCollection.cs

---
+++ ScheduleKeeper.Base/Types/RangeEnabledObservableCollection.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScheduleKeeper.Base.Types;
+
+public class RangeEnabledObservableCollection<T> : ObservableCollection<T>
+{
+    public void AddRange(IEnumerable<T> items)
+    {
+        CheckReentrancy();
+        foreach (var item in items)
+            Items.Add(item);
+        OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, items));
+    }
+
+    public void Remove(IEnumerable<T> items)
+    {
+        CheckReentrancy();
+        foreach (var item in items)
+            Items.Remove(item);
+        OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Remove, items));
+    }
+
+    public void ClearAndRepopulate(IEnumerable<T> items)
+    {
+        CheckReentrancy();
+        Items.Clear();
+        foreach (var item in items)
+            Items.Add(item);
+        OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset, items));
+    }
+}
