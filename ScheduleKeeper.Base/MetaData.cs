80588c7564c04e9da709f724cdceb23060ddbc5f
Diego García <dagarciam1014@gmail.com>
2021-09-15 00:40:54 -0400

    Created WPF and Base projects

---
+++ ScheduleKeeper.Base/MetaData.cs
@@ -0,0 +1,14 @@
+﻿global using System;
+global using System.Linq;
+global using System.Collections.Generic;
+global using DiegoG.Utilities;
+global using DiegoG.Utilities.IO;
+global using DiegoG.Utilities.Settings;
+global using System.Threading.Tasks;
+
+namespace ScheduleKeeper.Base
+{
+    public static class Meta
+    {
+    }
+}
---
+++ ScheduleKeeper.WPF/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿global using System;
+global using System.Linq;
+global using System.Collections.Generic;
+global using DiegoG.Utilities;
+global using DiegoG.Utilities.IO;
+global using DiegoG.Utilities.Settings;
+global using System.Threading.Tasks;
+global using ScheduleKeeper.Base;
+
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ScheduleKeeper.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
