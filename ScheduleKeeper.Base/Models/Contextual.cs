130e537b093870a432ea7c194368011bfeca109a
Diego García <dagarciam1014@gmail.com>
2021-09-15 01:53:53 -0400

    Created DescribedContextual

--- ScheduleKeeper.Base/Models/Contextual.cs
+++ ScheduleKeeper.Base/Models/Contextual.cs
@@ -1,16 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ScheduleKeeper.Base.Models
+namespace ScheduleKeeper.Base.Models;
+
+public abstract class Contextual : INotifyPropertyChanged
 {
-    public abstract class Contextual : INotifyPropertyChanged
+    public event PropertyChangedEventHandler? PropertyChanged;
+    protected void Notify([CallerMemberName] string? caller = null) => PropertyChanged?.Invoke(this, new(caller));
+}
+
+public abstract class DescribedContextual : Contextual
+{
+    private string _title;
+    private string? _description;
+    private ObservableCollection<MutableKeyValuePair<string, string>> _notes = new();
+
+    public string Title
     {
-        public event PropertyChangedEventHandler? PropertyChanged;
-        protected void Notify([CallerMemberName] string? caller = null) => PropertyChanged?.Invoke(this, new(caller));
+        get => _title;
+        set
+        {
+            if (value is null)
+                throw new ArgumentNullException(nameof(value), $"{nameof(Title)} cannot be set to null.");
+            if (value == _title)
+                return;
+            _title = value;
+            Notify();
+        }
     }
+
+    public string? Description
+    {
+        get => _description;
+        set
+        {
+            if (value == _description)
+                return;
+            _description = value;
+            Notify();
+        }
+    }
+
+    public ObservableCollection<MutableKeyValuePair<string, string>> Notes
+    {
+        get => _notes;
+        set
+        {
+            if (value is null)
+                throw new ArgumentNullException(nameof(value), $"{nameof(Notes)} cannot be set to null.");
+            if (ReferenceEquals(_notes, value))
+                return;
+            _notes = value;
+            Notify();
+        }
+    }
+
+    public DescribedContextual(string title) => _title = title;
 }
