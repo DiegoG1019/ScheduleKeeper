0bfbf1424d89801ffbf27c08575dfecde932d530
Diego García <dagarciam1014@gmail.com>
2021-09-18 23:44:51 -0400

    Create TimeFramePlanView

---
+++ ScheduleKeeper.WPF/Controls/TimeFramePlanView.xaml.cs
@@ -0,0 +1,25 @@
+﻿using ScheduleKeeper.Base.Models;
+using System.Windows;
+
+namespace ScheduleKeeper.WPF.Controls;
+/// <summary>
+/// Interaction logic for TimeFramePlan.xaml
+/// </summary>
+public partial class TimeFramePlanView 
+{
+    private bool _showDuration;
+    public bool ShowDuration
+    {
+        get => _showDuration;
+        set
+        {
+            if (_showDuration != value)
+                GridDuration.Height = (_showDuration = value) ? new GridLength(1, GridUnitType.Star) : new GridLength(0, GridUnitType.Star);
+        }
+    }
+
+    public TimeFramePlanView()
+    {
+        InitializeComponent();
+    }
+}
