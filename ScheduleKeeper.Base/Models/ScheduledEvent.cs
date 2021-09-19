a72f09924e731470a12a1720f6419cf708f852eb
Diego García <dagarciam1014@gmail.com>
2021-09-18 23:46:04 -0400

    Update ScheduledEvent.cs

--- ScheduleKeeper.Base/Models/ScheduledEvent.cs
+++ ScheduleKeeper.Base/Models/ScheduledEvent.cs
@@ -1,11 +1,34 @@
-﻿using System.Collections.ObjectModel;
+﻿using NeoSmart.AsyncLock;
+using Quartz;
+using Quartz.Impl;
+using System;
+using System.Collections.ObjectModel;
 using System.Collections.Specialized;
 
 namespace ScheduleKeeper.Base.Models;
 
-public class ScheduledEvent : DescribedContextual
+public class ScheduledEvent : DescribedContextual, IObservable<ScheduledEventNotification>
 {
+    private readonly static object hashlock = new();
+    private static int statichash;
+    
+    private readonly int hash;
+    public override int GetHashCode() => hash;
+
+    public ScheduledEvent(string title) : base(title)
+    {
+        lock (hashlock)
+            hash = statichash++;
+        SchedulerLock = new AsyncLock();
+        ActiveDays = new(_activeDays);
+        Plans.CollectionChanged += PlansCollectionChanged;
+    }
+
     public IEnumerable<TimeFramePlan> this[DayOfWeek dayOfWeek] => GetPlans(dayOfWeek);
+
+    #region fields
+
+    private readonly AsyncLock SchedulerLock;
 
     private readonly RangeEnabledObservableCollection<DayOfWeek> _activeDays = new();
 
@@ -19,19 +42,31 @@ public class ScheduledEvent : DescribedContextual
     private IEnumerable<TimeFramePlan>? _saturdayPlans;
     private int? _planCount;
 
+    #endregion
+
+    #region properties
+    private readonly object planlock = new();
     public ObservableCollection<TimeFramePlan> Plans
     {
-        get => _plans;
+        get
+        {
+            lock (planlock)
+                return _plans;
+        }
         set
         {
             if (value is null)
                 throw new ArgumentNullException(nameof(value), $"{nameof(Plans)} cannot be set to null.");
-            if (ReferenceEquals(_plans, value))
-                return;
+            lock (planlock)
+            {
+                if (ReferenceEquals(_plans, value))
+                    return;
 
-            _plans.CollectionChanged -= PlansCollectionChanged;
-            _plans = value;
-            _plans.CollectionChanged += PlansCollectionChanged;
+                _plans.CollectionChanged -= PlansCollectionChanged;
+                _plans = value;
+                _plans.CollectionChanged += PlansCollectionChanged;
+            }
+            _activeDays.AddRange(_plans.Select(x => x.OnDay).Distinct());
             ClearPlans();
             Notify();
         }
@@ -49,11 +84,11 @@ public class ScheduledEvent : DescribedContextual
     public IEnumerable<TimeFramePlan> FridayPlans => _fridayPlans ??= FetchPlans(DayOfWeek.Friday);
     public IEnumerable<TimeFramePlan> SaturdayPlans => _saturdayPlans ??= FetchPlans(DayOfWeek.Saturday);
 
-    public ScheduledEvent(string title) : base(title)
-    {
-        ActiveDays = new(_activeDays);
-        Plans.CollectionChanged += PlansCollectionChanged;
-    }
+    #endregion
+
+    #region methods
+
+    #region public
 
     public IEnumerable<TimeFramePlan> GetPlans(DayOfWeek dayOfWeek)
         => dayOfWeek switch
@@ -69,6 +104,10 @@ public class ScheduledEvent : DescribedContextual
                 $"The allowed range is between 0 and 6, or from {nameof(DayOfWeek.Sunday)} to {nameof(DayOfWeek.Saturday)}"),
         };
 
+    #endregion
+
+    #region protected
+
     protected string GetPlansDay(DayOfWeek dayOfWeek)
         => dayOfWeek switch
         {
@@ -82,50 +121,6 @@ public class ScheduledEvent : DescribedContextual
             _ => throw new ArgumentOutOfRangeException(nameof(dayOfWeek),
                 $"The allowed range is between 0 and 6, or from {nameof(DayOfWeek.Sunday)} to {nameof(DayOfWeek.Saturday)}"),
         };
-
-    private readonly Queue<DayOfWeek> DaysToInvalidate = new();
-    protected void PlansCollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
-    {
-        IEnumerable<DayOfWeek> affected;
-        switch (e.Action)
-        {
-            case NotifyCollectionChangedAction.Add:
-                affected = e.NewItems!.Cast<TimeFramePlan>().Select(x => x.OnDay).Distinct();
-                foreach (var p in affected)
-                    DaysToInvalidate.Enqueue(p);
-                _activeDays.AddRange(affected.Except(ActiveDays));
-                break;
-            case NotifyCollectionChangedAction.Remove:
-                affected = e.OldItems!.Cast<TimeFramePlan>().Select(x => x.OnDay).Distinct();
-                foreach (var p in affected)
-                    DaysToInvalidate.Enqueue(p);
-                _activeDays.Remove(affected);
-                break;
-            case NotifyCollectionChangedAction.Replace:
-                affected = e.OldItems!.Cast<TimeFramePlan>().Concat(e.NewItems!.Cast<TimeFramePlan>()).Select(x => x.OnDay).Distinct();
-                foreach (var p in affected)
-                    DaysToInvalidate.Enqueue(p);
-                _activeDays.ClearAndRepopulate(affected);
-                break;
-            case NotifyCollectionChangedAction.Reset:
-                foreach (var p in Enum.GetValues<DayOfWeek>())
-                    DaysToInvalidate.Enqueue(p);
-                _activeDays.Clear();
-                break;
-            case NotifyCollectionChangedAction.Move:
-                break;
-            default:
-                break;
-        }
-
-        _planCount = null;
-
-        while (DaysToInvalidate.Count > 0)
-        {
-            var p = DaysToInvalidate.Dequeue();
-            SetPlans(p, null);
-        }
-    }
 
     protected IEnumerable<TimeFramePlan> FetchPlans(DayOfWeek dayOfWeek)
         => Plans.Where(s => s.OnDay == dayOfWeek).ToArray();
@@ -164,5 +159,210 @@ public class ScheduledEvent : DescribedContextual
     {
         GetPlansRef(dayOfWeek) = plans;
         Notify(GetPlansDay(dayOfWeek));
+    }
+
+    #endregion
+
+    #region event handlers
+
+    private readonly Queue<DayOfWeek> DaysToInvalidate = new();
+    protected void PlansCollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
+    {
+        IEnumerable<DayOfWeek> affected;
+        IEnumerable<TimeFramePlan> frames;
+        switch (e.Action)
+        {
+            case NotifyCollectionChangedAction.Add:
+                affected = (frames = e.NewItems!.Cast<TimeFramePlan>()).Select(x => x.OnDay).Distinct();
+                foreach (var p in affected)
+                    DaysToInvalidate.Enqueue(p);
+                foreach (var f in frames)
+                    RegisterEvent(f);
+                _activeDays.AddRange(affected.Except(ActiveDays));
+                break;
+            case NotifyCollectionChangedAction.Remove:
+                affected = (frames = e.OldItems!.Cast<TimeFramePlan>()).Select(x => x.OnDay).Distinct();
+                foreach (var p in affected)
+                    DaysToInvalidate.Enqueue(p);
+                foreach (var f in frames)
+                    UnregisterEvent(f);
+                _activeDays.Remove(affected);
+                break;
+            case NotifyCollectionChangedAction.Replace:
+                var tr = e.OldItems!.Cast<TimeFramePlan>();
+                var ta = e.NewItems!.Cast<TimeFramePlan>();
+                affected = tr.Concat(ta).Select(x => x.OnDay).Distinct();
+                foreach (var p in affected)
+                    DaysToInvalidate.Enqueue(p);
+                foreach (var f in ta)
+                    RegisterEvent(f);
+                foreach (var f in tr)
+                    UnregisterEvent(f);
+                _activeDays.ClearAndRepopulate(affected);
+                break;
+            case NotifyCollectionChangedAction.Reset:
+                foreach (var p in Enum.GetValues<DayOfWeek>())
+                    DaysToInvalidate.Enqueue(p);
+                _activeDays.Clear();
+                ClearEventsAndRepopulate();
+                break;
+            case NotifyCollectionChangedAction.Move:
+                break;
+            default:
+                break;
+        }
+
+        _planCount = null;
+
+        while (DaysToInvalidate.Count > 0)
+        {
+            var p = DaysToInvalidate.Dequeue();
+            SetPlans(p, null);
+        }
+    }
+
+    #endregion
+
+    #endregion
+
+    #region IObservable
+
+    private readonly AsyncLock alock = new();
+    private readonly List<IObserver<ScheduledEventNotification>> Observers = new();
+
+    public IDisposable Subscribe(IObserver<ScheduledEventNotification> observer)
+    {
+        if (!Observers.Contains(observer))
+            using (alock.Lock())
+                Observers.Add(observer);
+        return new Unsubscriber(Observers, observer, alock);
+    }
+
+    public async Task<IAsyncDisposable> SubscribeAsync(IObserver<ScheduledEventNotification> observer)
+    {
+        if (!Observers.Contains(observer))
+            using (await alock.LockAsync())
+                Observers.Add(observer);
+        return new Unsubscriber(Observers, observer, alock);
+    }
+
+    protected async Task NotifyEventToObservers(ScheduledEventNotification eventNotification)
+    {
+        using (await alock.LockAsync())
+            foreach (var o in Observers)
+                try
+                {
+                    o.OnNext(eventNotification);
+                }
+                catch
+                {
+                    continue;
+                }
+    }
+
+    #region Unsubscriber
+    private class Unsubscriber : IDisposable, IAsyncDisposable
+    {
+        private readonly List<IObserver<ScheduledEventNotification>> Observers;
+        private readonly IObserver<ScheduledEventNotification> Observer;
+        private readonly AsyncLock AsyncLock;
+
+        public Unsubscriber(List<IObserver<ScheduledEventNotification>> observers, IObserver<ScheduledEventNotification> observer, AsyncLock asyncLock)
+        {
+            Observers = observers;
+            Observer = observer;
+            AsyncLock = asyncLock;
+        }
+
+        public void Dispose()
+        {
+            using (AsyncLock.Lock())
+                if (Observer != null && Observers.Contains(Observer))
+                    Observers.Remove(Observer);
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            using (await AsyncLock.LockAsync())
+                if (Observer != null && Observers.Contains(Observer))
+                    Observers.Remove(Observer);
+        }
+    }
+
+    #endregion
+
+    #endregion
+
+    #region Scheduler
+
+    private class SchedulerJob
+    {
+        private static int _hash = 0;
+        private static readonly object sync = new();
+
+        private int hash;
+        private readonly ScheduledEventNotification notif;
+        private readonly Timer timer;
+
+        public readonly ScheduledEvent Parent;
+        public readonly TimeFramePlan Plan;
+
+        public SchedulerJob(ScheduledEvent sched, TimeFramePlan plan)
+        {
+            lock (sync)
+                hash = _hash++;
+            notif = new(sched, plan);
+            Parent = sched;
+            Plan = plan;
+            timer = new(Callback, null, plan.StartTime - TimeOnly.FromDateTime(DateTime.Now), TimeSpan.FromHours(24));
+        }
+
+        public SchedulerJob Stop()
+        {
+            timer.Dispose();
+            return this;
+        }
+
+        ~SchedulerJob()
+            => Stop();
+
+        public override int GetHashCode() => hash;
+        private async void Callback(object? state) => await Parent.NotifyEventToObservers(notif);
+    }
+
+    private readonly List<SchedulerJob> Jobs = new();
+    private async void RegisterEvent(TimeFramePlan plan)
+    {
+        using (await alock.LockAsync())
+            if (!Jobs.Any(x => ReferenceEquals(plan, x.Plan)))
+                Jobs.Add(new SchedulerJob(this, plan));
+    }
+
+    private async void UnregisterEvent(TimeFramePlan plan)
+    {
+        SchedulerJob? job;
+        using (await alock.LockAsync())
+            if ((job = Jobs.FirstOrDefault(x => ReferenceEquals(plan, x.Plan))) is not null)
+                Jobs.Remove(job.Stop());
+    }
+
+    private async void ClearEventsAndRepopulate()
+    {
+        using (await alock.LockAsync())
+        {
+            foreach (var job in Jobs)
+                job.Stop();
+            Jobs.Clear();
+            foreach (var plan in Plans)
+                Jobs.Add(new(this, plan));
+        }
+    }
+
+    #endregion
+
+    ~ScheduledEvent()
+    {
+        foreach (var o in Observers)
+            o.OnCompleted();
     }
 }
