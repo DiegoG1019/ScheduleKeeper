3eb45602bf2449c7770b041f64828a65904c3124
Diego García <dagarciam1014@gmail.com>
2021-09-18 23:44:29 -0400

    Create ScheduleKeeperConfig.cs

---
+++ ScheduleKeeper.WPF/Types/ScheduleKeeperConfig.cs
@@ -0,0 +1,26 @@
+﻿global using ScheduleKeeper.WPF.Types;
+
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ScheduleKeeper.WPF.Types;
+
+public class ScheduleKeeperConfig : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler? PropertyChanged;
+    protected void Notify([CallerMemberName] string? caller = null)
+        => PropertyChanged?.Invoke(this, new(caller));
+    
+    /// <summary>
+    /// Throws if null, returns true if not the same
+    /// </summary>
+    protected static bool VerifyChange(object property, object value)
+        => value is not null && !ReferenceEquals(property, value);
+
+
+    /// <summary>
+    /// Throws if null, returns true if not the same and thus should notify a change
+    /// </summary>
+    protected static bool SetAndVerifyChange<T>(ref T property, T value)
+        => value is not null && !ReferenceEquals(property, property = value);
+}
