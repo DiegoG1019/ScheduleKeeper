24ad8d54536c436051f3c8f4039a41515d5e51e3
Diego García <dagarciam1014@gmail.com>
2021-09-18 23:44:34 -0400

    Create Settings.cs

---
+++ ScheduleKeeper.WPF/Settings.cs
@@ -0,0 +1,36 @@
+﻿namespace ScheduleKeeper.WPF;
+
+public sealed class Config : ScheduleKeeperConfig, ISettings
+{
+    private const string _st = $"{nameof(ScheduleKeeper)}.{nameof(WPF)}.{nameof(Config)}";
+    public string SettingsType { get; } = _st;
+    public ulong Version { get; }
+
+    //----
+    private ScheduledEventSettings_ _scheduledEventSettings = new();
+
+    public ScheduledEventSettings_ ScheduledEventSettings
+    {
+        get => _scheduledEventSettings;
+        set
+        {
+            if (SetAndVerifyChange(ref _scheduledEventSettings, value))
+                Notify();
+        }
+    }
+
+    public sealed class ScheduledEventSettings_ : ScheduleKeeperConfig
+    {
+        private bool tfp_showduration;
+
+        public bool ShowTimeFrameDuration
+        {
+            get => tfp_showduration;
+            set
+            {
+                if (SetAndVerifyChange(ref tfp_showduration, value))
+                    Notify();
+            }
+        }
+    }
+}
