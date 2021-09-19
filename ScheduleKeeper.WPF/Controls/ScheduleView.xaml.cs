c295ba40b65d35fbe3442b27d3646af23580868b
Diego García <dagarciam1014@gmail.com>
2021-09-18 23:45:02 -0400

    Began work towards ScheduleView

---
+++ ScheduleKeeper.WPF/Controls/ScheduleView.xaml.cs
@@ -0,0 +1,22 @@
+﻿using ScheduleKeeper.Base.Models;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ScheduleKeeper.WPF.Controls;
+/// <summary>
+/// Interaction logic for ScheduleView.xaml
+/// </summary>
+public partial class ScheduleView : UserControl
+{
+    private Schedule? _dat;
+    private Schedule Context => _dat ??= (DataContext as Schedule) ?? throw new InvalidOperationException($"DataContext is not a {nameof(Schedule)}");
+
+    private void DataContextChangedHandler(object sender, DependencyPropertyChangedEventArgs e)
+        => _dat = null;
+
+    public ScheduleView()
+    {
+        InitializeComponent();
+        DataContextChanged += DataContextChangedHandler;
+    }
+}
